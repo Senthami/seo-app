32cf9f585a1019fcec8aa4cc62a70b3952af5452
Senthamizh <senthamizh@taxida.in>
2022-04-05 18:09:22 +0530

    Second commit

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ function HomePage() {
         <title>My page title</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <p>Hello world!</p>
+      <p>Hello world</p>
       <div className="product-links">
         {[...new Array(5)].map((i, index) => (
           <Link href={`/products/${index + 1}`}>
